6453a94ea4699a648b93a38bd944fa6d0f6bb1b7
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2025-07-05 22:59:52 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3b82f6",
+        dark: "#111827",
+      },
+    },
+  },
+  plugins: [],
+}
